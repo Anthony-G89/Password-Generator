7695511e37e88c7c1a5f26e88d7d6a6461af13ce
Anthony Guerrero <adguerrero1935@gmail.com>
2020-01-15 01:02:27 -0500

    added some if statement

--- Script.js
+++ Script.js
@@ -7,38 +7,27 @@ var upperCase= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
 var Numbers= [0,1,2,3,4,5,6,7,8,9,]
 var Symbols= ['!','@','#','$','%','^','&','*','(',')','_','+'];
 
-
 var length= confirm('Password must be 8-128 characters.');
-var lowerCaseQuestion= confirm('Do you want a lower case character.');
-var upperCaseQuestion= confirm('Do you want a upper case character.');
+var lowerCaseQuestion= confirm('Do you want a lowercase character.');
+var upperCaseQuestion= confirm('Do you want a uppercase character.');
 var numberQuestion= confirm('Do you want a Number.');
 var SymbolsQuestion= confirm('Do you want to add a symbol.');
 
 if (lowerCaseQuestion){
-    alert('Lower case will be added'); //yes
+    alert('Lowercase will be added'); 
 }
-else if (upperCaseQuestion){
-    alert('Do you want to add a upper case character.'); //no
-}
-
-if(upperCaseQuestion){
-    alert('An uppercase will be added.'); //yes
-}
-else if (SymbolsQuestion){
-    alert(' do you want to add a number') //no
+if (upperCaseQuestion){
+    alert('Uppercase will be added');
 }
 
 if (numberQuestion){
     alert('A number will be added.');
 }
-else if (SymbolsQuestion){
-    alert
-
-}
 
 if (SymbolsQuestion){
     alert('A symbol will be added.');
 }
+
 
 function generatePassword(){
 
