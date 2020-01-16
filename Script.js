cbadd0e157d369394075b7581ad69a4f9bbf54a9
Anthony Guerrero <adguerrero1935@gmail.com>
2020-01-16 13:41:01 -0500

    added possibleCharacters and .concat

--- Script.js
+++ Script.js
@@ -2,35 +2,46 @@
 var generateBtn = document.querySelector("#generate");
 
 
+
+function generatePassword(){
+
+  var possibleCharacters= [];
+console.log(possibleCharacters);
+
 var lowerCase= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var Numbers= [0,1,2,3,4,5,6,7,8,9,]
 var Symbols= ['!','@','#','$','%','^','&','*','(',')','_','+'];
 
-var length= confirm('Password must be 8-128 characters.');
+var length= prompt('How many character would you like.');
 var lowerCaseQuestion= confirm('Do you want a lowercase character.');
 var upperCaseQuestion= confirm('Do you want a uppercase character.');
 var numberQuestion= confirm('Do you want a Number.');
 var SymbolsQuestion= confirm('Do you want to add a symbol.');
 
 if (lowerCaseQuestion){
-    alert('Lowercase will be added'); 
+    possibleCharacters=possibleCharacters.concat(lowerCase); 
 }
 if (upperCaseQuestion){
-    alert('Uppercase will be added');
+    possibleCharacters=possibleCharacters.concat(upperCase); 
 }
 
 if (numberQuestion){
-    alert('A number will be added.');
+  possibleCharacters=possibleCharacters.concat(Numbers); 
 }
 
 if (SymbolsQuestion){
-    alert('A symbol will be added.');
+  possibleCharacters=possibleCharacters.concat(Symbols); 
 }
 
+console.log(possibleCharacters);
 
-function generatePassword(){
+// for loop to the length of that varible
 
+for ( i=0; i < length; i++){
+
+
+}
 
 
 
@@ -77,3 +88,11 @@ function copyToClipboard() {
 generateBtn.addEventListener("click", writePassword);
 
 // BONUS EVENT LISTENER
+for (let i = 0; i < 10; i++) {
+  var CA_rps = ["r", "p", "s"];
+var rv = Math.floor(Math.random()*CA_rps.length);
+cChoice=CA_rps[rv];
+console.log(cChoice);
+
+  
+}
