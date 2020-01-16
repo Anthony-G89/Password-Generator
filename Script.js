8210b60294984662f8d58ebe3c5d2ab2ee5581a4
Anthony Guerrero <adguerrero1935@gmail.com>
2020-01-16 14:34:07 -0500

    updated for loop

--- Script.js
+++ Script.js
@@ -5,9 +5,7 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
 
-  var possibleCharacters= [];
-console.log(possibleCharacters);
-
+ var possibleCharacters= [];
 var lowerCase= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var Numbers= [0,1,2,3,4,5,6,7,8,9,]
@@ -36,35 +34,20 @@ if (SymbolsQuestion){
 
 console.log(possibleCharacters);
 
+
+
+
 // for loop to the length of that varible
 
 for ( i=0; i < length; i++){
-
+  password = password + lowerCase, upperCase,Numbers,Symbols,(Math.floor(Math.random()* Math.floor(lowerCase,upperCase,Numbers,Symbols.length -1)));
 
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  return "this is my password"
+  return   document.getElementById("#password") ,lowerCase,upperCase,Numbers,Symbols= password
 }
 
 
@@ -88,11 +71,11 @@ function copyToClipboard() {
 generateBtn.addEventListener("click", writePassword);
 
 // BONUS EVENT LISTENER
-for (let i = 0; i < 10; i++) {
-  var CA_rps = ["r", "p", "s"];
-var rv = Math.floor(Math.random()*CA_rps.length);
-cChoice=CA_rps[rv];
-console.log(cChoice);
+//for (let i = 0; i < 10; i++) {
+  //var CA_rps = ["r", "p", "s"];
+//var rv = Math.floor(Math.random()*CA_rps.length);
+//cChoice=CA_rps[rv];
+//console.log(cChoice);
 
   
-}
+//}
