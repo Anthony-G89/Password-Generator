7bfa027b0f07c52f6c58438dd41b21709beb6ec9
Anthony Guerrero <adguerrero1935@gmail.com>
2020-01-17 10:26:54 -0500

    updated JS

--- Script.js
+++ Script.js
@@ -32,22 +32,29 @@ if (SymbolsQuestion){
   possibleCharacters=possibleCharacters.concat(Symbols); 
 }
 
-console.log(possibleCharacters);
-
-
-
-
 // for loop to the length of that varible
 
 for ( i=0; i < length; i++){
-  password = password + lowerCase, upperCase,Numbers,Symbols,(Math.floor(Math.random()* Math.floor(lowerCase,upperCase,Numbers,Symbols.length -1)));
 
+  var userRps= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','!','@','#','$','%','^','&','*','(',')','_','+',0,1,2,3,4,5,6,7,8,9];
+  var formula= Math.floor(Math.random() * userRps.length);
+  option=userRps[formula];
+  //console.log(option);
+  
+
+
+
+
+
+  //password = password + lowerCase,upperCase,Numbers,Symbols,(Math.floor(Math.random()* Math.floor(lowerCase,upperCase,Numbers,Symbols.length -1)));
+      //password = upperCase[Math.floor(Math.random() * upperCase.length)];
+      
+    
+    
+      
 }
 
-
-
-
-  return   document.getElementById("#password") ,lowerCase,upperCase,Numbers,Symbols= password
+  return 
 }
 
 
