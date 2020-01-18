26a6c4de1e1c573a4745d0abc80246a1c6af638f
Anthony Guerrero <adguerrero1935@gmail.com>
2020-01-18 12:37:24 -0500

    updated working script

--- Script.js
+++ Script.js
@@ -1,60 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var newPassword = [];
+  var possibleCharacters = [];
+  var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var Numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
+  var Symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
+
+  var length = prompt('How many character would you like.');
 
 
-function generatePassword(){
 
- var possibleCharacters= [];
-var lowerCase= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCase= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var Numbers= [0,1,2,3,4,5,6,7,8,9,]
-var Symbols= ['!','@','#','$','%','^','&','*','(',')','_','+'];
+  var lowerCaseQuestion = confirm('Do you want a lowercase character.');
+  var upperCaseQuestion = confirm('Do you want a uppercase character.');
+  var numberQuestion = confirm('Do you want a Number.');
+  var SymbolsQuestion = confirm('Do you want to add a symbol.');
 
-var length= prompt('How many character would you like.');
-var lowerCaseQuestion= confirm('Do you want a lowercase character.');
-var upperCaseQuestion= confirm('Do you want a uppercase character.');
-var numberQuestion= confirm('Do you want a Number.');
-var SymbolsQuestion= confirm('Do you want to add a symbol.');
 
-if (lowerCaseQuestion){
-    possibleCharacters=possibleCharacters.concat(lowerCase); 
-}
-if (upperCaseQuestion){
-    possibleCharacters=possibleCharacters.concat(upperCase); 
-}
+  
+  if (lowerCaseQuestion) {
+    possibleCharacters = possibleCharacters.concat(lowerCase);
+  }
+  if (upperCaseQuestion) {
+    possibleCharacters = possibleCharacters.concat(upperCase);
+  }
 
-if (numberQuestion){
-  possibleCharacters=possibleCharacters.concat(Numbers); 
-}
+  if (numberQuestion) {
+    possibleCharacters = possibleCharacters.concat(Numbers);
+  }
 
-if (SymbolsQuestion){
-  possibleCharacters=possibleCharacters.concat(Symbols); 
-}
+  if (SymbolsQuestion) {
+    possibleCharacters = possibleCharacters.concat(Symbols);
+  }
 
-// for loop to the length of that varible
+  // for loop to the length of that varible
 
-for ( i=0; i < length; i++){
 
-  var userRps= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','!','@','#','$','%','^','&','*','(',')','_','+',0,1,2,3,4,5,6,7,8,9];
-  var formula= Math.floor(Math.random() * userRps.length);
-  option=userRps[formula];
-  //console.log(option);
+  for (var i = 0; i < length; i++) {
+
+    var randomIndex = Math.floor(Math.random() * possibleCharacters.length);
+    newPassword.push(possibleCharacters[randomIndex]);
+  }
   
 
-
-
-
-
-  //password = password + lowerCase,upperCase,Numbers,Symbols,(Math.floor(Math.random()* Math.floor(lowerCase,upperCase,Numbers,Symbols.length -1)));
-      //password = upperCase[Math.floor(Math.random() * upperCase.length)];
-      
-    
-    
-      
-}
-
-  return 
+  return newPassword.join("");
 }
 
 
@@ -84,5 +75,5 @@ generateBtn.addEventListener("click", writePassword);
 //cChoice=CA_rps[rv];
 //console.log(cChoice);
 
-  
+
 //}
